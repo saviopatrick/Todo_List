a9a4abcf3137e3e39ca2e7a5c6b14f0990b612fa
SavioPatrick <saviopatrickc@gmail.com>
2024-01-08 18:43:39 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,38 @@ import { useState } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "Criar funcionalidade x no sistema",
+      category:"Trabalho",
+      isCompleted:false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category:"Pessoal",
+      isCompleted:false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category:"Estudar",
+      isCompleted:false,
+    },
     
   ])
   return (
-   <div>
-    ola
+   <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) =>(
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
    </div>
   )
 }
