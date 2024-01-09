b12a7a4478314ab2075b4721905ca838b537d047
SavioPatrick <saviopatrickc@gmail.com>
2024-01-09 17:40:32 -0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import "./App.css";
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 
 function App() {
@@ -23,18 +28,50 @@ function App() {
     },
     
   ])
+
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("ALL");
+  const [sort, setSort] = useState("Asc")
+
+  const addTodo = (text, category) => {
+
+    const newTodo = [
+      ...todos,
+      {
+        id: Math.floor(Math.random()*10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodo);
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+    setTodos(newTodos);
+
+  }
+
+
   return (
    <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
-        {todos.map((todo) =>(
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-            </div>
-          </div>
+        {todos.filter((todo) => filter ==="ALL" ? true: filter === "Completed" ? todo.isCompleted : !todo.isCompleted).filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).sort((a,b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text)).map((todo) =>(
+         <Todo key= {todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
+      <TodoForm addTodo={addTodo}/>
    </div>
   )
 }
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,34 @@
+import {createFactory, useState} from 'react'
+
+const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(!value || !category) return;
+        addTodo(value,category)
+        setValue("");
+        setCategory("");
+    };
+    
+
+  return (
+    <div>
+        <h2>Criar Terefa</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Digite o titulo' onChange={(e) => setValue(e.target.value)} value={value}/>
+
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type="submit">Criar Tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
